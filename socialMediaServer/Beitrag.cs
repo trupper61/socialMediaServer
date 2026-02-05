1462806a3315c04c558c12ace7d5a46235c4e9f5
trupper61 <rupert.hoffmann.06@gmail.com>
2026-02-05 17:40:57 +0100

    added classes (still unfinished)

---
+++ socialMediaServer/Beitrag.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace socialMediaServer
+{
+    public class Beitrag
+    {
+        private DateTime geposted;
+        private string titel;
+        private int anzahlLikes;
+        private Nutzer autor;
+        private Text text;
+
+        public Beitrag(Nutzer autor, string titel)
+        {
+            this.autor = autor;
+            this.titel = titel;
+            anzahlLikes = 0;
+            geposted = DateTime.Now;
+            // Bild hinzufuegen
+        }
+
+        public void Hinzufuegen()
+        {
+            // Bild
+        }
+
+        public void ErstelleText(string text)
+        {
+            this.text = new Text(text);
+        }
+
+        public void Like()
+        {
+            anzahlLikes++;
+        }
+    }
+}
---
+++ socialMediaServer/Nutzer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace socialMediaServer
+{
+    public class Nutzer
+    {
+        private string benutzerName;
+        private string passwort;
+        private string email;
+        private DateTime zuletztAktiv;
+        private List<Nutzer> abonnenten;
+        private List<Nutzer> abonnierteNutzer;
+        private List<Beitrag> beitraege;
+        // bilder Liste
+
+        public Nutzer(string name, string passwort, string email)
+        {
+            this.benutzerName = name;
+            this.passwort = passwort;
+            this.email = email;
+            zuletztAktiv = DateTime.Now;
+            abonnenten = new List<Nutzer>();
+            abonnierteNutzer = new List<Nutzer>();
+            beitraege = new List<Beitrag>();
+
+        }
+
+
+
+    }
+}
